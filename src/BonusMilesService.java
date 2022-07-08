651570850596dd0d493f86e00cd9d59b5fd66106
Viacheslav Ryabov <vermulion@gmail.com>
2022-07-08 22:51:53 +0300

    Changed formula

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,8 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-    int bonus; // начисляемые бонусные рубли
-        if (cost > 1000) {
-        bonus = (int) (0.01 * cost);
-    } else {
-        bonus = 0;
+    int miles = cost/20;
+        return miles;
     }
-        return bonus;
+
 }
-    }
+
