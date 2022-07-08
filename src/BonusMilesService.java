bf986fb7f8cda9b0a7f6597d73a4cdea18b5bc99
Viacheslav Ryabov <vermulion@gmail.com>
2022-07-08 06:58:27 +0300

    Created

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,3 +1,11 @@
 public class BonusMilesService {
-
+    public int calculate(int price) {
+    int bonus; // начисляемые бонусные рубли
+        if (price > 1000) {
+        bonus = (int) (0.01 * price);
+    } else {
+        bonus = 0;
+    }
+        return bonus;
 }
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
-ppublic class Main {
+public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price);
-        System.out.println(miles);
+        int account = 656; // начальный аккаунт
+        int price = 10_000; // сумма пополнения
+        int miles = service.calculate(price); // начисляемые бонусные рубли
+        int final_account = account + price + miles; // итоговый счет после пополнения
+
+        String rub = " руб.";
+        String acc = "Баланс счета: ";
+        String gratification = "Начисленные бонусные рубли: ";
+        System.out.println(gratification + miles + rub);
+        System.out.println(acc + final_account + rub);
     }
 }
