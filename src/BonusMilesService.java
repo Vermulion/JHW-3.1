c03dde29b025490920e751c13c53745643a2da1d
Viacheslav Ryabov <vermulion@gmail.com>
2022-07-08 14:22:22 +0300

    Cost added

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
-    public int calculate(int price) {
+    public int calculate(int cost) {
     int bonus; // начисляемые бонусные рубли
-        if (price > 1000) {
-        bonus = (int) (0.01 * price);
+        if (cost > 1000) {
+        bonus = (int) (0.01 * cost);
     } else {
         bonus = 0;
     }
